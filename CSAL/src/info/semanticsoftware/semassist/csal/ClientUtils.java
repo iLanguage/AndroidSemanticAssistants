52532e12d194cbe7f07e5e338ac47f6fb5a2914a
Bahar Sateli <baharsateli@users.noreply.github.com>
2010-12-07 16:16:28 +0000

    Parsing bug fixed #135

--- CSAL/src/info/semanticsoftware/semassist/csal/ClientUtils.java
+++ CSAL/src/info/semanticsoftware/semassist/csal/ClientUtils.java
@@ -28,7 +28,6 @@ import java.util.*;
 import java.text.DateFormat;
 
 import info.semanticsoftware.semassist.csal.result.*;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.w3c.dom.*;
