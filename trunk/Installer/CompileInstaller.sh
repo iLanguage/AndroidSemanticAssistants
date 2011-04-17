f5b4a3feed3be24be812bdb42471005f7df103b3
renew <renew@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2011-04-17 23:12:30 +0000

    license corrected

--- trunk/Installer/CompileInstaller.sh
+++ trunk/Installer/CompileInstaller.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp ../SemassistProperties.xml varfile
+cp ../SemassistProperties.xml varfile/
+cp ../LICENSE.txt .
 /usr/local/durmtools/IzPack/bin/compile install.xml -b . -o "Semantic Assistants Installer.jar" -k standard
