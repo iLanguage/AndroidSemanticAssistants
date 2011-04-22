9f5dcb1fece36f73ee3acd1051c8bd0a932ccb41
renew <renew@users.noreply.github.com>
2011-04-22 18:36:56 +0000

    integrated JAX-WS panel into Installer

--- Installer/CompileInstaller.sh
+++ Installer/CompileInstaller.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#! /bin/bash
 
+# copy current SemassistProperties.xml and configure variables
 cp ../SemassistProperties.xml varfile/
+sed -i varfile/SemassistProperties.xml -e "s/\${durmtools}\/GATE\/gate/\$gate.location.dir/"
+sed -i varfile/SemassistProperties.xml -e "s/\${durmtools}\/jaxws-ri/\$jaxws.location.dir/"
+
+# copy current LICENSE text
 cp ../LICENSE.txt .
-/usr/local/durmtools/IzPack/bin/compile install.xml -b . -o "Semantic Assistants Installer.jar" -k standard
+
+# create installer
+/usr/local/durmtools/IzPack/bin/compile install.xml -b . -o "SemanticAssistantsInstaller.jar" -k standard
