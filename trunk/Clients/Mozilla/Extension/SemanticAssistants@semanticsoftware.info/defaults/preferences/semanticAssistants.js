e720588d82f7b9bc675c469d9244a712f8dc4487
jtans <jtans@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2011-06-22 07:44:33 +0000

    Mozilla (Firefox and Thunderbird) extension - initial commit

---
+++ trunk/Clients/Mozilla/Extension/SemanticAssistants@semanticsoftware.info/defaults/preferences/semanticAssistants.js
@@ -0,0 +1,5 @@
+pref("extensions.semanticAssistants.firstTimeRun", true);
+pref("extensions.semanticAssistants.installLocation", "");
+pref("extensions.semanticAssistants.serverCustomHost", "");
+pref("extensions.semanticAssistants.serverCustomPort", "8879");
+pref("extensions.semanticAssistants.serverDefaultOrCustom", 0);
