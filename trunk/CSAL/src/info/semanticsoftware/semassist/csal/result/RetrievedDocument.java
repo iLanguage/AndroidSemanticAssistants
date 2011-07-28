83853bf4ccc138b11699d266a8d0310fc310c8ae
renew <renew@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2011-07-28 21:12:03 +0000

    changing the CSAL license to LGPL3 #165

--- trunk/CSAL/src/info/semanticsoftware/semassist/csal/result/RetrievedDocument.java
+++ trunk/CSAL/src/info/semanticsoftware/semassist/csal/result/RetrievedDocument.java
@@ -3,21 +3,19 @@
 
     This file is part of the Semantic Assistants architecture.
 
-    Copyright (C) 2009 Semantic Software Lab, http://www.semanticsoftware.info
-        Nikolaos Papadakis
-        Tom Gitzinger
+    Copyright (C) 2009, 2010, 2011 Semantic Software Lab, http://www.semanticsoftware.info
 
     The Semantic Assistants architecture is free software: you can
-    redistribute and/or modify it under the terms of the GNU Affero General
+    redistribute and/or modify it under the terms of the GNU Lesser General
     Public License as published by the Free Software Foundation, either
     version 3 of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+    GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
@@ -25,7 +23,6 @@ package  info.semanticsoftware.semassist.csal.result;
 
 public class RetrievedDocument
 {
-     
      public String url;
           
 }
