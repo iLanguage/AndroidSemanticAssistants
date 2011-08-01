6d0942e155fa6a2a3c833e90cf94e02a9be39176
baharsateli <baharsateli@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2011-08-01 20:31:02 +0000

    Added FIXME flag for unused variable

--- trunk/CSAL/src/info/semanticsoftware/semassist/csal/ClientUtils.java
+++ trunk/CSAL/src/info/semanticsoftware/semassist/csal/ClientUtils.java
@@ -160,6 +160,7 @@ public class ClientUtils
         }
 
         Node root = xmlDoc.getDocumentElement();
+        //FIXME check for unused variable here
         Vector<SemanticServiceResult> results = getServiceResults( root );
 
         // Work with node.getLocalName()
