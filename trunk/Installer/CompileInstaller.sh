d53189d15c13ededcc5734ab12712a39d532c1fd
renew <renew@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2011-08-06 14:18:33 +0000

    Removed default server/port dialog from Installer (#171), as this will now be handled by a user preferences file (#205). Closes #171.

--- trunk/Installer/CompileInstaller.sh
+++ trunk/Installer/CompileInstaller.sh
@@ -4,8 +4,8 @@
 cp ../SemassistProperties.xml varfile/
 sed -i varfile/SemassistProperties.xml -e "s/\${durmtools}\/GATE\/gate/\$gate.location.dir/"
 sed -i varfile/SemassistProperties.xml -e "s/\${durmtools}\/jaxws-ri/\$jaxws.location.dir/"
-sed -i varfile/SemassistProperties.xml -e "s/assistant.cs.concordia.ca/\$CSAL.Server.Name/"
-sed -i varfile/SemassistProperties.xml -e "s/8879/\$CSAL.Server.Port/"
+#sed -i varfile/SemassistProperties.xml -e "s/assistant.cs.concordia.ca/\$CSAL.Server.Name/"
+#sed -i varfile/SemassistProperties.xml -e "s/8879/\$CSAL.Server.Port/"
 
 # copy current LICENSE text
 cp ../LICENSE.txt .
