ab7d3a816c2009306a956e8e5f835928448a9d00
Bahar Sateli <bahar.sateli@gmail.com>
2012-04-11 17:12:36 -0400

    Updated encryption utils

--- src/info/semanticsoftware/semassist/android/encryption/EncryptionUtils.java
+++ src/info/semanticsoftware/semassist/android/encryption/EncryptionUtils.java
@@ -3,9 +3,7 @@ package info.semanticsoftware.semassist.android.encryption;
 import info.semanticsoftware.semassist.android.prefs.PrefUtils;
 
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 
@@ -15,7 +13,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import android.util.Base64;
@@ -56,150 +53,17 @@ public class EncryptionUtils {
 		}
 		return key;
 	}
-	
-	/**
-	 * Encrypts the secret session key with the saved public key
-	 * so that nobody except the server can use it to encrypt
-	 * the service output.
-	 * @param sessionKey secret session key
-	 * @return encrypted secret session key string
-	 * */
-	public String encryptSessionKey(byte[] sessionKey){
-		String encryptedSessionKeyString = null;
-		//STEP 1: Get the public key component and create it from configuration
-		PrefUtils prefUtils = PrefUtils.getInstance();
-		PublicKey pubKey = prefUtils.getPublicKey();
-		
-		//STEP 2: Use the public key to encrypt the secret session key
-		try {
-			Cipher cipher;
-			/*
-			 With CBC or CFB modes, you must set an IV before encrypting or decrypting. 
-			 Because there are no interblock dependencies in ECB mode, you do not need to set an IV. 
-			 In fact, if you try to use the setIV() or generateIV() methods with a symmetric cipher 
-			 that uses ECB mode, you will get an exception.
-			 see http://www.rsa.com/products/bsafe/documentation/cryptoj35html/doc/dev_guide/group__CJ__SYM__ECB.html
-			 */
-			cipher = Cipher.getInstance("RSA/ECB/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE,pubKey);
-			byte[] cipherDataBytes = cipher.doFinal(sessionKey);
-			encryptedSessionKeyString = Base64.encodeToString(cipherDataBytes, Base64.DEFAULT);
-			return encryptedSessionKeyString;
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return encryptedSessionKeyString;
-	}
-	
-	/**
-	 * Encrypts the secret session key with the saved public key
-	 * so that nobody except the server can use it to encrypt
-	 * the service output.
-	 * @param sessionKey secret session key
-	 * @return encrypted secret session key string
-	 * */
-	public String encryptData(byte[] input){
-		String encryptedSessionKeyString = null;
-		//STEP 1: Get the public key component and create it from configuration
-		PrefUtils prefUtils = PrefUtils.getInstance();
-		PublicKey pubKey = prefUtils.getPublicKey();
-		
-		//STEP 2: Use the public key to encrypt the secret session key
-		try {
-			Cipher cipher;
-			cipher = Cipher.getInstance("RSA/ECB/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE,pubKey);
-			byte[] cipherDataBytes = cipher.doFinal(input);
-			encryptedSessionKeyString = Base64.encodeToString(cipherDataBytes, Base64.DEFAULT);
-			return encryptedSessionKeyString;
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return encryptedSessionKeyString;
-	}
-	
-	/* client-side
-	// encryptedText and IV will come from server, we have secretKey in the client
-	public String decryptMessage(String encryptedText, byte[] secretSessionKey, byte[] iv){
-		String strOriginal = "";
-		try {
-			SecretKeySpec keySpec = new SecretKeySpec(secretSessionKey, "AES");
-			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-			cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv));
-			byte[] original = cipher.doFinal(Base64.decode(encryptedText, Base64.DEFAULT));
-			strOriginal = new String(original, "UTF-8");
-			return strOriginal;
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		}
-		return strOriginal;
-	}*/
-	
-	/**
-	 * Encrypts the input data using the provided public key
-	 * @param input data string to encrypt
-	 * @param key public key to use in encryption
-	 * @return byte[] encrypted byte array
-	 * */
-	public byte[] encryptData(String input, PublicKey key){
-		byte[] cipherData = null;
-		try {
-			Cipher cipher;
-			cipher = Cipher.getInstance("RSA/ECB/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE,key);
-			cipherData = cipher.doFinal(input.getBytes("UTF-8"));
-			return cipherData;
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
 
-	public byte[] encryptInputData(String input, byte[] sessionKey) {
+	public String encryptInputData(String input, byte[] sessionKey) {
 		byte[] encryptedText = null;
 		try {
-			Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 			SecretKeySpec keySpec = new SecretKeySpec(sessionKey, "AES");
 			cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+			siv = cipher.getIV();
 			encryptedText = cipher.doFinal(input.getBytes("UTF-8"));
+			return Base64.encodeToString(encryptedText, Base64.DEFAULT);
+			
 		} catch (InvalidKeyException e) {
 			e.printStackTrace();
 		} catch (IllegalBlockSizeException e) {
@@ -213,8 +77,111 @@ public class EncryptionUtils {
 		} catch (NoSuchPaddingException e) {
 			e.printStackTrace();
 		}	
-		return encryptedText;
+		return "";
+	}
+	
+	public String getIV(){
+		return Base64.encodeToString(siv, Base64.DEFAULT);
+	}
+	
+	//////////////////
+	
+	public String encryptSessionKey(byte[] sessionKey){
+		String encryptedSessionKeyString = null;
+		//STEP 1: Get the public key component and create it from configuration
+		PrefUtils prefUtils = PrefUtils.getInstance();
+		PublicKey pubKey = prefUtils.getPublicKey();
+		
+		//STEP 2: Use the public key to encrypt the secret session key
+		try {
+			/*
+			 With CBC or CFB modes, you must set an IV before encrypting or decrypting. 
+			 Because there are no interblock dependencies in ECB mode, you do not need to set an IV. 
+			 In fact, if you try to use the setIV() or generateIV() methods with a symmetric cipher 
+			 that uses ECB mode, you will get an exception.
+			 see http://www.rsa.com/products/bsafe/documentation/cryptoj35html/doc/dev_guide/group__CJ__SYM__ECB.html
+			 */
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE,pubKey);
+			byte[] cipherDataBytes = cipher.doFinal(sessionKey);
+			encryptedSessionKeyString = Base64.encodeToString(cipherDataBytes, Base64.DEFAULT);
+			System.out.println("dbg: " + encryptedSessionKeyString);
+			return encryptedSessionKeyString;
+		} catch (NoSuchPaddingException e) {	
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return encryptedSessionKeyString;
+	}
+
+	public String encryptTest() {
+		PrefUtils prefUtils = PrefUtils.getInstance();
+		PublicKey pubKey = prefUtils.getPublicKey();
+		System.out.println("PublicKey remade: " + pubKey);
+		try {
+			
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE,pubKey);
+			byte[] cipherDataBytes = cipher.doFinal("This is a test".getBytes("UTF-8"));
+			String encryptedSessionKeyString = Base64.encodeToString(cipherDataBytes, Base64.DEFAULT);
+			return encryptedSessionKeyString;
+		} catch (NoSuchPaddingException e) {	
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	public String encryptMe(byte[] sessionKey) {
+		String encryptedSessionKeyString = null;
+		//STEP 1: Get the public key component and create it from configuration
+		PrefUtils prefUtils = PrefUtils.getInstance();
+		PublicKey pubKey = prefUtils.getPublicKey();
+		
+		//STEP 2: Use the public key to encrypt the secret session key
+		try {
+			/*
+			 With CBC or CFB modes, you must set an IV before encrypting or decrypting. 
+			 Because there are no interblock dependencies in ECB mode, you do not need to set an IV. 
+			 In fact, if you try to use the setIV() or generateIV() methods with a symmetric cipher 
+			 that uses ECB mode, you will get an exception.
+			 see http://www.rsa.com/products/bsafe/documentation/cryptoj35html/doc/dev_guide/group__CJ__SYM__ECB.html
+			 */
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE,pubKey);
+			byte[] cipherDataBytes = cipher.doFinal(sessionKey);
+			encryptedSessionKeyString = Base64.encodeToString(cipherDataBytes, Base64.DEFAULT);
+			return encryptedSessionKeyString;
+		} catch (NoSuchPaddingException e) {	
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return encryptedSessionKeyString;
 	}
 	
 }
-
