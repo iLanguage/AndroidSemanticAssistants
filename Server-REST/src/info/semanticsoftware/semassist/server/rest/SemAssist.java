62c44e468e462247e9490de41a07fb19bd5fb490
Bahar Sateli <baharsateli@users.noreply.github.com>
2012-04-16 19:16:10 +0000

    User licensing

--- Server-REST/src/info/semanticsoftware/semassist/server/rest/SemAssist.java
+++ Server-REST/src/info/semanticsoftware/semassist/server/rest/SemAssist.java
@@ -1,3 +1,26 @@
+/*	
+Semantic Assistants -- http://www.semanticsoftware.info/semantic-assistants
+
+This file is part of the Semantic Assistants architecture.
+
+Copyright (C) 2012, 2013 Semantic Software Lab, http://www.semanticsoftware.info
+Rene Witte
+Bahar Sateli
+
+The Semantic Assistants architecture is free software: you can
+redistribute and/or modify it under the terms of the GNU Affero General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package info.semanticsoftware.semassist.server.rest;
 
 import info.semanticsoftware.semassist.server.rest.resource.ServiceResource;
