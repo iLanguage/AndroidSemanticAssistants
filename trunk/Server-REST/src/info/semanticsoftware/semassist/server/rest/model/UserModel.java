48862aa360e2b6bd99cd1e639106117837942462
baharsateli <baharsateli@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2012-04-16 15:44:44 +0000

    User data model

---
+++ trunk/Server-REST/src/info/semanticsoftware/semassist/server/rest/model/UserModel.java
@@ -0,0 +1,42 @@
+package info.semanticsoftware.semassist.server.rest.model;
+
+import java.util.Iterator;
+
+import info.semanticsoftware.semassist.server.ServiceInfoForClient;
+import info.semanticsoftware.semassist.server.core.security.authentication.AuthenticationUtils;
+
+public class UserModel {
+	
+	Iterator<ServiceInfoForClient> iterator = null;
+    
+	public UserModel(){
+
+	}
+	
+	/**
+	 * Returns XML representation of a user
+	 * @param userName username
+	 * @return XML representation of the specified user with public key value or null
+	 */
+	public String getXML(String userName) {
+		StringBuilder xml = new StringBuilder();
+		xml.append("<user>");
+		xml.append("<userName>").append(userName).append("</userName>");
+		
+		xml.append("<userKey>").append(AuthenticationUtils.getInstance().getModulusString(userName)).append("</userKey>");
+		
+		xml.append("<link>").append(getLink(userName)).append("</link>");
+		xml.append("</user>");
+		return xml.toString();
+	}
+	
+	/**
+	 * Returns partial link URI of the specified user
+	 * @param userName username
+	 * @return partial URI for the user
+	 */
+	private String getLink(String userName){
+		return "/users/" + userName;
+	}
+
+}
