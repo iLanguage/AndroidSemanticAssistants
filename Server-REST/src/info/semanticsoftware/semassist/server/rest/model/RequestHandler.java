113243bc4a59208974b9f52f4179692b85faec1f
Bahar Sateli <baharsateli@users.noreply.github.com>
2012-04-16 15:43:47 +0000

    Encryption and authentication added to RESTful interface

--- Server-REST/src/info/semanticsoftware/semassist/server/rest/model/RequestHandler.java
+++ Server-REST/src/info/semanticsoftware/semassist/server/rest/model/RequestHandler.java
@@ -1,25 +1,3 @@
-/*
-Semantic Assistants -- http://www.semanticsoftware.info/semantic-assistants
-
-This file is part of the Semantic Assistants architecture.
-
-Copyright (C) 2012, 2013 Semantic Software Lab, http://www.semanticsoftware.info
-Rene Witte
-Bahar Sateli
-
-The Semantic Assistants architecture is free software: you can
-redistribute and/or modify it under the terms of the GNU Affero General
-Public License as published by the Free Software Foundation, either
-version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package info.semanticsoftware.semassist.server.rest.model;
 
 import org.xml.sax.Attributes;
@@ -27,12 +5,27 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class RequestHandler extends DefaultHandler{
+	// ===========================================================
+    // Fields
+    // ===========================================================
    
     private boolean serviceNameTag = false;
     private boolean inputTag = false;
+    private boolean usernameTag = false;
+    private boolean passwordTag = false;
+    private boolean sessionKeyTag = false;
+    private boolean sessionIVTag = false;
     private String serviceName;
     private String input;
+    private String username;
+    private String password;
+    private String sessionKey;
+    private String sessionIV;
    
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+
     public String getServiceName(){
     	return this.serviceName;
     }
@@ -40,7 +33,27 @@ public class RequestHandler extends DefaultHandler{
     public String getInput(){
     	return this.input;
     }
+    
+    public String getUsername(){
+    	return this.username;
+    }
+    
+    public String getPassword(){
+    	return this.password;
+    }
+    
+    public String getSessionKeyString(){
+    	return this.sessionKey;
+    }
+    
+    public String getSessionIVString(){
+    	return this.sessionIV;
+    }
 
+
+    // ===========================================================
+    // Methods
+    // ===========================================================
     @Override
     public void startDocument() throws SAXException {
     	System.out.println("Started parsing service request");
@@ -53,30 +66,61 @@ public class RequestHandler extends DefaultHandler{
     	System.out.println("Finished parsing service request");
     }
 
+    /** Gets called on opening tags like:
+     * <tag>
+     * Can provide attribute(s), when xml was like:
+     * <tag attribute="attributeValue">*/
     @Override
     public void startElement(String namespaceURI, String localName,String qName, Attributes atts) throws SAXException {
             if (qName.equals("serviceName")) {
                  this.serviceNameTag = true;
             }else if (qName.equals("input")) {
                 this.inputTag = true;
+	        }else if (qName.equals("username")) {
+	        	this.usernameTag = true;
+	        }else if (qName.equals("password")) {
+	        	this.passwordTag = true;
+	        }else if (qName.equals("sessionKey")) {
+	        	this.sessionKeyTag = true;
+	        }else if (qName.equals("sessionIV")) {
+	        	this.sessionIVTag = true;
 	        }
     }
-   
+    /** Gets called on closing tags like:
+     * </tag> */
     @Override
     public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
     	if (qName.equals("serviceName")) {
             this.serviceNameTag = false;
     	}else if (qName.equals("input")) {
-        this.inputTag = false;
-    	}
+    		this.inputTag = false;
+    	}else if (qName.equals("username")) {
+    		this.usernameTag = false;
+    	}else if (qName.equals("password")) {
+    		this.passwordTag = false;
+    	}else if (qName.equals("sessionKey")) {
+    		this.sessionKeyTag = false;
+    	}else if (qName.equals("sessionIV")) {
+        	this.sessionIVTag = false;
+        }
     }
    
+    /** Gets called on the following structure:
+     * <tag>characters</tag> */
     @Override
     public void characters(char ch[], int start, int length) {
             if(this.serviceNameTag){
             	serviceName = new String(ch, start, length);
             }else if(this.inputTag){
-            	input = new String(ch, start, length);
+            	input = new String(ch, start, length).trim();
+            }else if(this.usernameTag){
+            	username = new String(ch, start, length);
+            }else if(this.passwordTag){
+            	password = new String(ch, start, length).trim();
+            }else if(this.sessionKeyTag){
+            	sessionKey = new String(ch, start, length).trim();
+            }else if(this.sessionIVTag){
+            	sessionIV = new String(ch, start, length).trim();
             }
     }
 }
---
+++ Server-REST/src/info/semanticsoftware/semassist/server/security/encryption/EncryptionUtils.java
@@ -0,0 +1,203 @@
+package info.semanticsoftware.semassist.server.core.security.encryption;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class EncryptionUtils {
+	
+	private static EncryptionUtils instance = null;
+	private final static String algorithm = "RSA";
+	private final static int keySize = 4096;
+	
+	private RSAPublicKeySpec pubKeySpec = null;
+	private RSAPrivateKeySpec priKeySpec = null;
+	
+	protected EncryptionUtils(){
+		// Defeat instantiation
+	}
+	
+	public static EncryptionUtils getInstance(){
+		if ( instance == null ){
+			instance = new EncryptionUtils();
+		}
+		
+		return instance;
+	}
+	
+
+	/**
+	 * Generates a key-pair based on the specified algorithm
+	 * @return KeyPair or null if an exception is thrown
+	 */
+	public KeyPair generateKeyPair(){
+		KeyPair pair = null;
+		KeyPairGenerator kpg;
+		try {
+			kpg = KeyPairGenerator.getInstance(algorithm);
+			kpg.initialize(keySize);
+			
+			pair = kpg.generateKeyPair();
+			return pair;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return pair;
+	}
+	
+	/**
+	 * Extracts a public key from the provided KeyPair
+	 * @param pair a key-pair
+	 * @return PublicKey or null
+	 */
+	public PublicKey getPublicKey(KeyPair pair){
+		PublicKey pubKey = null;
+		try {
+			PublicKey publicKey = pair.getPublic();
+			KeyFactory factory = KeyFactory.getInstance(algorithm);
+			pubKeySpec = factory.getKeySpec(publicKey, RSAPublicKeySpec.class);
+			pubKey = factory.generatePublic(pubKeySpec);
+			return pubKey;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		return pubKey;
+	}
+	
+	/**
+	 * Extracts a private key from the provided KeyPair
+	 * @param pair a key-pair
+	 * @return PrivateKey or null
+	 */
+	public PrivateKey getPrivateKey(KeyPair pair){
+		PrivateKey priKey = null;
+		try {
+			PrivateKey privateKey = pair.getPrivate();
+			KeyFactory factory = KeyFactory.getInstance(algorithm);
+			priKeySpec = factory.getKeySpec(privateKey, RSAPrivateKeySpec.class);
+			priKey = factory.generatePrivate(priKeySpec);
+			return priKey;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		return priKey;
+	}
+	
+	public BigInteger getModulus(PublicKey key){
+		return pubKeySpec.getModulus();
+		
+	}
+	
+	public BigInteger getPubEx(PublicKey key){
+		return pubKeySpec.getPublicExponent();
+	}
+	
+	public BigInteger getPriEx(PrivateKey key){
+		return priKeySpec.getPrivateExponent();
+	}
+
+	public String decryptInputData(String input, byte[] sessionKey, byte[] siv){
+		String decryptedText = null;
+		try {
+			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+			SecretKeySpec keySpec = new SecretKeySpec(sessionKey, "AES");
+			cipher.init(Cipher.DECRYPT_MODE,keySpec, new IvParameterSpec(siv));
+			byte[] decryptedData = cipher.doFinal(Base64.decodeBase64(input));
+			decryptedText = new String(decryptedData, "UTF-8");
+			return decryptedText;
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return decryptedText;
+	}
+	
+	////////////////
+	
+	public byte[] decryptSessionKey(String cipherData, PrivateKey key){
+		byte[] original = null;
+		try {
+			Cipher cipher;
+			cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			original = cipher.doFinal(Base64.decodeBase64(cipherData));
+			
+			SecretKey secretKey =  new SecretKeySpec (original, "AES");
+			return secretKey.getEncoded();
+			//return original;
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return original;
+	}
+
+	public String decryptTest(String test, PrivateKey key) {
+		try {
+			Cipher cipher;
+			cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] original = cipher.doFinal(Base64.decodeBase64(test));
+			
+			return new String(original, "UTF-8");
+			//return original;
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
