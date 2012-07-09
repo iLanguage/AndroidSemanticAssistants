2e7dfba7e392c5144147a9b6fbe1d9b14b5b8912
Bahar Sateli <baharsateli@users.noreply.github.com>
2012-07-09 16:35:54 +0000

    Fixed checkstyle warnings

--- Server-REST/src/info/semanticsoftware/semassist/server/rest/utils/ClientUtils.java
+++ Server-REST/src/info/semanticsoftware/semassist/server/rest/utils/ClientUtils.java
@@ -1,3 +1,25 @@
+/*
+Semantic Assistants -- http://www.semanticsoftware.info/semantic-assistants
+
+This file is part of the Semantic Assistants architecture.
+
+Copyright (C) 2012, 2013 Semantic Software Lab, http://www.semanticsoftware.info
+Rene Witte
+Bahar Sateli
+
+The Semantic Assistants architecture is free software: you can
+redistribute and/or modify it under the terms of the GNU Affero General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package info.semanticsoftware.semassist.server.rest.utils;
 
 import java.io.StringWriter;
@@ -13,17 +35,36 @@ import org.restlet.resource.ClientResource;
  *  */
 public class ClientUtils {
 
-	  /**
-	 * Creates an XML representation for a service invocation request.
-	 * @param serviceName NLP service name to invoke
-	 * @param params list of runtime parameters
-	 * @param inputString input string for analysis
-	 * @param authenticationNeeded authentication mode
-	 * @param username username (only needed in authentication mode)
-	 * @param password password (only needed in authentication mode)
-	 * @throws Exception if user credentials are invalid
-	 * @return XML representation of the request
-	 * */
+	/** class singleton object. */
+	private static ClientUtils instance = null;
+
+	/** Protected constructor. **/
+	protected ClientUtils(){
+		// defeat instantiation
+	}
+
+	/**
+	 * Returns the class singleton object.
+	 * @return singleton object
+	 */
+	public static ClientUtils getInstance(){
+		if(instance == null){
+			instance = new ClientUtils();
+		}
+		return instance;
+	}
+
+	/**
+	* Creates an XML representation for a service invocation request.
+	* @param serviceName NLP service name to invoke
+	* @param params list of runtime parameters
+	* @param inputString input string for analysis
+	* @param authenticationNeeded authentication mode
+	* @param username username (only needed in authentication mode)
+	* @param password password (only needed in authentication mode)
+	* @throws Exception if user credentials are invalid
+	* @return XML representation of the request
+	* */
 	public static String createXMLServiceRequest(final String serviceName, final Map<String, String> params,
 			final String inputString, final boolean authenticationNeeded, final String username, final String password) throws Exception{
 		StringBuffer buffer = new StringBuffer();
