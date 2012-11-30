b77e894a7ca7dce705435e2bdfdc0798c3928ee2
baharsateli <baharsateli@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2012-11-30 02:00:46 +0000

    Removing the Mozilla project from the public repo

--- trunk/Clients/Mozilla/Extension/content/SAUtils.js
+++
@@ -1,7 +0,0 @@
-var SAUtils = {
-	/* log to console */
-	log: function(message) {
-		var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
-		consoleService.logStringMessage("Semantic Assistants: " + message);
-	}
-};
