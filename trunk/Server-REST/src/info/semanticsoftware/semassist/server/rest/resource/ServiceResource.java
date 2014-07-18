21a2dd995c8dc753de6909b893ffe027643b7c52
baharsateli <baharsateli@faee4525-dc5d-4c7a-abb1-b4ae5a78574a>
2014-07-18 22:08:35 +0000

    Added representation of services in JSON format

--- trunk/Server-REST/src/info/semanticsoftware/semassist/server/rest/resource/ServiceResource.java
+++ trunk/Server-REST/src/info/semanticsoftware/semassist/server/rest/resource/ServiceResource.java
@@ -3,7 +3,7 @@ Semantic Assistants -- http://www.semanticsoftware.info/semantic-assistants
 
 This file is part of the Semantic Assistants architecture.
 
-Copyright (C) 2012, 2013 Semantic Software Lab, http://www.semanticsoftware.info
+Copyright (C) 2013, 2014 Semantic Software Lab, http://www.semanticsoftware.info
 Rene Witte
 Bahar Sateli
 
@@ -19,7 +19,7 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package info.semanticsoftware.semassist.server.rest.resource;
 
@@ -27,6 +27,9 @@ import java.io.IOException;
 
 import info.semanticsoftware.semassist.server.rest.model.RequestParser;
 import info.semanticsoftware.semassist.server.rest.model.ServiceModel;
+import info.semanticsoftware.semassist.server.rest.utils.Constants;
+
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
@@ -35,47 +38,84 @@ import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
-/** Router class to execute service resource requests.
+/**
+ * Router class to execute service resource requests.
+ * 
  * @author Bahar Sateli
  */
-public class ServiceResource extends ServerResource{
+public class ServiceResource extends ServerResource {
 
 	/**
-	 * Handles HTTP GET requests. Each request is passed
-	 * to the ServiceModel class to find the corresponding
-	 * service object. The results is an XML representation
-	 * of the service.
-	 * @return XML representation of the service object
+	 * Handles HTTP GET requests. Each request is passed to the ServiceModel
+	 * class to find the corresponding service object. The results is an XML or
+	 * JSON representation of the service.
+	 * 
+	 * @return XML or JSON representation of the service object
 	 * */
-	@Get("xml")
-	public Representation getXML() {
-		String serviceName = (String) getRequest().getAttributes().get("serviceName");
-		StringRepresentation representation = null;	
-		String xml = new ServiceModel().getXML(serviceName);
-		representation = new StringRepresentation(xml, MediaType.APPLICATION_XML);
+	@Get
+	public Representation getRepresentation() {
+		String serviceName = (String) getRequest().getAttributes().get(
+				"serviceName");
+		// FIXME make proper URL encoding
+		serviceName = serviceName.replace("%20", " ");
 
-		if (xml != null) {
-			return representation;
-		} else {
-			setStatus(Status.CLIENT_ERROR_NOT_FOUND);
-			return null;
+		StringRepresentation representation = null;
+
+		// What MIME types does the client accept?
+		Form headers = (Form) getRequestAttributes().get(
+				"org.restlet.http.headers");
+		String accepts = headers.getFirstValue("Accept");
+		// if no MIME provided, fall back to XML (for backward compatibility
+		// with other SA clients
+		accepts = (accepts == null) ? "xml" : accepts;
+		System.out.println("Request Accepts: " + accepts);
+
+		switch (Constants.MIME_TYPES.valueOf(accepts.toUpperCase())) {
+		case JSON:
+			String json = new ServiceModel().getJSON(serviceName);
+			representation = new StringRepresentation(json,
+					MediaType.APPLICATION_JSON);
+			break;
+
+		case XML:
+		case APPXHTML:
+		case APPXML:
+			String xml = new ServiceModel().getXML(serviceName);
+			if (xml == null) {
+				setStatus(Status.CLIENT_ERROR_NOT_FOUND);
+				return null;
+			}
+			representation = new StringRepresentation(xml,
+					MediaType.APPLICATION_XML);
+			break;
+		case TEXT:
+			// TODO send a plain text representation
+			break;
+		default:
+			break;
 		}
+		return representation;
+
 	}
 
-	/** Handles HTTP POST requests. Each requests is passed
-	 * to the handler class, which in eventually executes
-	 * an invocation call to the SA server. 
-	 * @param representation representation of the request
+	/**
+	 * Handles HTTP POST requests. Each requests is passed to the handler class,
+	 * which in eventually executes an invocation call to the SA server.
+	 * 
+	 * @param representation
+	 *            representation of the request
 	 * @return string representation of the server response
 	 * */
 	@Post
-	public StringRepresentation invoke(final Representation representation){
+	public StringRepresentation invoke(final Representation representation) {
 		try {
-			System.out.println("Request Content-Length size: " + representation.getSize() + " bytes.");
+			System.out.println("Request Content-Length size: "
+					+ representation.getSize() + " bytes.");
 			String request = representation.getText();
 			System.out.println("REQUEST: " + request);
 			RequestParser parser = new RequestParser(request);
-			StringRepresentation response = new StringRepresentation(parser.executeRequest());
+			StringRepresentation response = new StringRepresentation(
+					parser.executeRequest());
 			return response;
 		} catch (IOException e) {
 			e.printStackTrace();
--- trunk/Server-REST/src/info/semanticsoftware/semassist/server/rest/resource/ServicesResource.java
+++ trunk/Server-REST/src/info/semanticsoftware/semassist/server/rest/resource/ServicesResource.java
@@ -3,7 +3,7 @@ Semantic Assistants -- http://www.semanticsoftware.info/semantic-assistants
 
 This file is part of the Semantic Assistants architecture.
 
-Copyright (C) 2012, 2013 Semantic Software Lab, http://www.semanticsoftware.info
+Copyright (C) 2013, 2014 Semantic Software Lab, http://www.semanticsoftware.info
 Rene Witte
 Bahar Sateli
 
@@ -19,30 +19,68 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package info.semanticsoftware.semassist.server.rest.resource;
 
 import info.semanticsoftware.semassist.server.rest.model.ServiceModel;
-
+import info.semanticsoftware.semassist.server.rest.utils.Constants;
+import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
-/** Router class to execute service inquiry requests.
+/**
+ * Router class to execute service inquiry requests.
+ * 
  * @author Bahar Sateli
  */
-public class ServicesResource extends ServerResource{
+public class ServicesResource extends ServerResource {
 
-	/** Handles HTTP GET requests. Returns an XML
-	 * representation of the available services.
-	 * @return XML representation of available services */
-	@Get("xml")
-	public Representation getXML() {
-		String xml = new ServiceModel().getAllXML();
-		Representation representation = new StringRepresentation(xml, MediaType.APPLICATION_XML);
+	/**
+	 * Handles HTTP GET requests. Returns an XML or JSON representation of the
+	 * available services.
+	 * 
+	 * @return XML or JSON representation of available services
+	 */
+	@Get
+	public Representation getRepresentation() {
+		Representation representation = null;
+
+		// What MIME types does the client accept?
+		Form headers = (Form) getRequestAttributes().get(
+				"org.restlet.http.headers");
+		String accepts = headers.getFirstValue("Accept");
+		// if no MIME provided, fall back to XML (for backward compatibility
+		// with other SA clients
+		accepts = (accepts == null) ? "xml" : accepts;
+		System.out.println("Request Accepts: " + accepts);
+
+		switch (Constants.MIME_TYPES.valueOf(accepts.toUpperCase())) {
+		case JSON:
+			String json = new ServiceModel().getAllJSON();
+			representation = new StringRepresentation(json,
+					MediaType.APPLICATION_JSON);
+			break;
+
+		case XML:
+		case APPXHTML:
+		case APPXML:
+			String xml = new ServiceModel().getAllXML();
+			representation = new StringRepresentation(xml,
+					MediaType.APPLICATION_XML);
+			break;
+
+		case TEXT:
+			//TODO send a plain text representation
+			break;
+		default:
+			break;
+		}
 		return representation;
+
 	}
+
 }
